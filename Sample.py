fc754f79f520dd7e81788af8d8ae47ab037a74a7
edoalynne <edo.a.lynne@gmail.com>
2018-05-18 11:03:23 -0400

    roles

--- Sample.py
+++ Sample.py
@@ -15,6 +15,7 @@ async def on_message(message):
 		msg = "Hello " + message.content[4:] + ", I'm Dad"
 		await client.send_message(message.channel, msg)
 	
+	await client.send_message(message.channel, message.author.roles)
 	if "admin" in message.author.roles:
 		if message.content == "!Kill":
 			await client.send_message(message.channel, "Logging off...")
