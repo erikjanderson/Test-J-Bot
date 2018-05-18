2430d960241a804995f863a83c30d4a5301c940b
ITSerikjanderson <39337588+ITSerikjanderson@users.noreply.github.com>
2018-05-18 12:00:20 -0400

    Added Systems  online when bot starts up for the first time

--- Sample.py
+++ Sample.py
@@ -11,7 +11,12 @@ async def on_message(message):
 	if message.author == client.user:
 		return	
 
-	if message.content.startswith("I'm "):
+
+	if message.content.lower().startswith('!hello'):
+		msg = 'Hello {0.author.mention}'.format(message)
+		await client.send_message(message.channel, msg)
+
+	if message.content.lower().startswith("i'm "):
 		msg = "Hello " + message.content[4:] + ", I'm Dad"
 		await client.send_message(message.channel, msg)
 	
@@ -25,6 +30,8 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
+	channel = [channel for channel in client.get_all_channels() if channel.name == 'dev-test'][0]
+	await client.send_message(channel, "Systems Online...")
 	print('Logged in as')
 	print(client.user.name)
 	print(client.user.id)
